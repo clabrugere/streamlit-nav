30f8aad285c717ae1cbb039c1505fa68ab561740
Clement LABRUGERE <clement.labrugere@airliquide.com>
2021-06-02 13:24:52 +0200

    added static build of the component

--- navigation/__init__.py
+++ navigation/__init__.py
@@ -1,22 +1,8 @@
 import os
 import streamlit.components.v1 as components
 
-# Create a _RELEASE constant. We'll set this to False while we're developing
-# the component, and True when we're ready to package and distribute it.
-# (This is, of course, optional - there are innumerable ways to manage your
-# release process.)
+
 _RELEASE = False
-
-# Declare a Streamlit component. `declare_component` returns a function
-# that is used to create instances of the component. We're naming this
-# function "_component_func", with an underscore prefix, because we don't want
-# to expose it directly to users. Instead, we will create a custom wrapper
-# function, below, that will serve as our component's public API.
-
-# It's worth noting that this call to `declare_component` is the
-# *only thing* you need to do to create the binding between Streamlit and
-# your component frontend. Everything else we do in this file is simply a
-# best practice.
 
 if not _RELEASE:
     _component_func = components.declare_component(
@@ -30,13 +16,15 @@ else:
 
 
 def navigation(pages, default=None, key=None):
-
-    assert len(pages) > 0, "Must have at least one page"
+    
+    if len(pages) == 0:
+        raise ValueError("Must have at least one page")
+    
+    if default is not None and default not in pages:
+        raise ValueError("Default must be a page in 'pages'")
 
     if not default:
         default = pages[0]
-
-    assert default in pages, "Default must be a page name"
 
     return _component_func(
         pages=pages,
---
+++ navigation/frontend/build/precache-manifest.c531a37cc4d662b50f0a5e8b6318d664.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ab9759f3790a1db6154462291e2dca5",
+    "url": "./index.html"
+  },
+  {
+    "revision": "89adc97526fc0b7cc970",
+    "url": "./static/css/2.a7cecf69.chunk.css"
+  },
+  {
+    "revision": "89adc97526fc0b7cc970",
+    "url": "./static/js/2.64ccf170.chunk.js"
+  },
+  {
+    "revision": "481ffa692589faecfc9e92cb6d7fba92",
+    "url": "./static/js/2.64ccf170.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e83065ad3e066a03ad7d",
+    "url": "./static/js/main.8d13f616.chunk.js"
+  },
+  {
+    "revision": "7c26bca7e16783d14d15",
+    "url": "./static/js/runtime-main.11ec9aca.js"
+  }
+]);
